733b82144551f6363314e53fe862ab9b443fd425
Rodrigo Amarilla <amarillarodrigo14@gmail.com>
2024-08-07 20:38:58 -0300

    CRUD terminado

---
+++ database/db.js
@@ -0,0 +1,21 @@
+import mysql2 from 'mysql2/promise'
+
+export const connection = mysql2.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'tasks_db'
+});
+
+async function connectToDatabase() {
+    try {
+        const conn = await connection;
+        console.log('Connected as id ' + conn.threadId);
+        return conn;
+    } catch (err) {
+        console.error('Error connecting: ' + err.stack);
+        throw err;
+    }
+}
+
+connectToDatabase();
--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
-import express from "express";
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import tasksRoutes from '../routes/tasks_routes.js';
+
 
 const app = express();
-const PORT = 5000;
 
+//Middlewares
+app.use(cors())
+app.use(morgan('dev'))
 app.use(express.json())
 
+// Rutas
+app.use(tasksRoutes);
+
+//Configuracion del puerto
+const PORT = 3030;
+
+//Configuracion del server.
 app.listen(PORT, console.log('Servidor funcionando en el puerto:',PORT))
 
